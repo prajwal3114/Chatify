b1d95818fdd2486420554df52304305d498a643e
PRAJAWAL G <prajwal.gaddle@gmail.com>
2025-12-04 21:56:01 +0530

    Added the models and schemas forthe login section along with some part of backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import mongoose from 'mongoose'
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-  res.send('nodemon working Hello World!')
-})
-
+const URI = process.env.MONGODB_URI ;
+try {
+    mongoose.connect(URI)
+    console.log("Connected to MongoDB");
+    
+} catch (error) {
+    console.log(error);
+}
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
